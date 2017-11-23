d9fbc04cfc1f1ee3be56fc2a58dcee466c000b5e
Moiz <progrmoiz@gmail.com>
2017-11-23 23:41:01 +0500

    Added feature autocomplete

--- script/app.js
+++ script/app.js
@@ -1,29 +1,40 @@
-// autocompletion
 const searchInput = document.querySelector('.js-search-input');
-let timeout;
+const searchAuto = document.querySelector('.search__autocomplete > ul');
 
+// autocompletion
+let timeout;
 searchInput.addEventListener('keypress', function(e) {
+  // debouncing
   clearTimeout(timeout);
   timeout = setTimeout(() => {
-    console.log(e);
+    const query = queryParams({
+      "action": "opensearch",
+      "format": "json",
+      "search": e.target.value,
+      "origin": "*"
+    });
+    const fetchPromise = fetch(`https://en.wikipedia.org/w/api.php?${query}`);
+
+    fetchPromise
+    .then(res => {
+      return res.json();
+    })
+    .then(autocomplete => {
+      searchAuto.innerHTML = autocomplete[1]
+      .map(s => `<li class="aci">${s}</li>`)
+      .join('');
+      return searchAuto.querySelectorAll('li');
+    })
+    .then(list => {
+      list.forEach(li => {
+        li.addEventListener('click', function(e) {
+          searchInput.value = e.target.textContent;
+          // TODO: click search button
+        })
+      })
+    });
   }, 1000);
 })
-
-function request(url, options={}) {
-    options = {
-        // your default options
-        credentials: 'same-origin',
-        redirect: 'error',
-        ...options,
-    };
-
-    if(options.queryParams) {
-        url += (url.indexOf('?') === -1 ? '?' : '&') + queryParams(options.queryParams);
-        delete options.queryParams;
-    }
-
-    return fetch(url, options);
-}
 
 function queryParams(params) {
     return Object.keys(params)
