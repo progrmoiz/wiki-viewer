ac814f33f80a22111c1d982821327b3862177f28
Moiz <progrmoiz@gmail.com>
2017-11-25 02:27:08 +0500

    show empty state if no search value

--- script/app.js
+++ script/app.js
@@ -66,48 +66,55 @@ searchForm.addEventListener('submit', function(e) {
   });
   const fetchPromise = fetch(`https://en.wikipedia.org/w/api.php?${query}`);
   const tplSearch = document.querySelector('#tpl-article').innerHTML;
+  console.log(searchInput.value);
 
-  fetchPromise
-  .then(res => res.json())
-  .then(json => {
-    // remove previous articles
+  // if no search value show empty state
+  if (searchInput.value == '') {
     articles.innerHTML = '';
-    return Object.values(json.query.pages);
-  })
-  .then(pages => pages.sort((a, b) => a.index > b.index ? 1 : -1))
-  .then(pages => pages.map(page => {
-      const g = tplSearch
-      .replace('{{id}}', page.pageid)
-      .replace('{{heading}}', page.title)
-      .replace('{{content}}', page.extract)
-      .replace('{{has-image}}', page.thumbnail ? 'article--has-thumbnail' : '');
-
-      const a = document.createElement('a');
-      a.href = `https://en.wikipedia.org/?curid=${page.pageid}`;
-      a.title = page.title;
-      a.classList.add('al');
-      a.innerHTML = g;
-
-      if (page.thumbnail) {
-        const img = document.createElement('img');
-        img.src = page.thumbnail.source;
-        img.height = page.thumbnail.height;
-        img.width = page.thumbnail.width >= 90 ? 85 : page.thumbnail.width;
-        img.classList.add('article__thumbnail');
-        img.alt = page.title;
-
-        var parentNode = a.querySelector('.article');
-        var heading = a.querySelector('.article__heading');
-        parentNode.insertBefore(img, heading);
-      }
-
-      return a;
-    }))
-  .then(pages => {
-    pages.forEach(page => {
-      articles.appendChild(page);
+    articles.appendChild(emptyState);
+  } else {
+    fetchPromise
+    .then(res => res.json())
+    .then(json => {
+      // remove previous articles
+      articles.innerHTML = '';
+      return Object.values(json.query.pages);
     })
-  })
+    .then(pages => pages.sort((a, b) => a.index > b.index ? 1 : -1))
+    .then(pages => pages.map(page => {
+        const g = tplSearch
+        .replace('{{id}}', page.pageid)
+        .replace('{{heading}}', page.title)
+        .replace('{{content}}', page.extract)
+        .replace('{{has-image}}', page.thumbnail ? 'article--has-thumbnail' : '');
+
+        const a = document.createElement('a');
+        a.href = `https://en.wikipedia.org/?curid=${page.pageid}`;
+        a.title = page.title;
+        a.classList.add('al');
+        a.innerHTML = g;
+
+        if (page.thumbnail) {
+          const img = document.createElement('img');
+          img.src = page.thumbnail.source;
+          img.height = page.thumbnail.height;
+          img.width = page.thumbnail.width >= 90 ? 85 : page.thumbnail.width;
+          img.classList.add('article__thumbnail');
+          img.alt = page.title;
+
+          var parentNode = a.querySelector('.article');
+          var heading = a.querySelector('.article__heading');
+          parentNode.insertBefore(img, heading);
+        }
+
+        return a;
+      }))
+    .then(pages => {
+      pages.forEach(page => {
+        articles.appendChild(page);
+      })
+    })
+  }
 })
 
 function queryParams(params) {
